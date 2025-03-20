9dcf03f9e94494532b14259a6cc61f5cc18a3a34
5atriy0 <satriyobagus47@gmail.com>
2025-03-20 13:51:41 +0700

    Jobsheet 5 Tugas m_supplier

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -58,6 +58,14 @@
           <p>Transaksi Penjualan</p> 
         </a> 
       </li> 
+
+      <li class="nav-header">Data Supplier</li> 
+      <li class="nav-item"> 
+        <a href="{{ url('/supplier') }}" class="nav-link {{ ($activeMenu == 'supplier')? 'active' : '' }} "> 
+          <i class="nav-icon fas fa-warehouse"></i> 
+          <p>Data Supplier</p> 
+        </a> 
+      </li> 
     </ul> 
   </nav> 
 </div> 
